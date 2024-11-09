0eef1cdc93e19a6e776b3d829c3d92a13d4b5fb7
jiayi <jiayizhan2005@gmail.com>
2024-11-09 16:54:50 +0100

    Created navigation bar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
-console.log("Script ready");
+const body = document.body;
+const select = (e) => document.querySelector(e);
+const selectAll = (e) => document.querySelectorAll(e);
+
+initScript();
+
+/**
+ * Init all scripts
+ */
+function initScript() {
+  initNavMenu();
+}
+
+/**
+ * Hamburger Nav Open/Close
+ * Link Managed
+ */
+function initNavMenu() {
+  $(document).ready(function () {
+    $("aside .burger").click(function () {
+      if ($("aside .burger").hasClass("active")) {
+        $("aside .burger").removeClass("active");
+        $(".body-wrapper").removeClass("nav-active");
+      } else {
+        $("aside .burger").addClass("active");
+        $(".body-wrapper").addClass("nav-active");
+      }
+    });
+    $(".mobile-nav a").click(function (e) {
+      e.preventDefault();
+      if ($("aside .burger").hasClass("active")) {
+        $("aside .burger").removeClass("active");
+        $(".body-wrapper").removeClass("nav-active");
+      }
+    });
+  });
+}
